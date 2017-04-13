a3eee7f77199be6b4a3a2e4ae2cb352770d90b8b
Ekta3008 <ep00348130@techmahindra.com>
2017-04-13 13:31:11 +0530

    Ekta-Update

---
+++ Java_Project/src/java_package/Class1.java
@@ -0,0 +1,11 @@
+package java_package;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("First Update");
+		System.out.println("Second Update");
+
+	}
+
+}
